8fa2af81e76cad9fb75ab429f07f294650275458
gabo <gabrielbarragan07@gmail.com>
2021-04-11 21:25:29 -0500

    cambio en la url del script index.js y de link a la hoja de estilos

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+
 
 const url = 'https://platzi-avo.vercel.app/api/avo'
 const urlbase= 'https://platzi-avo.vercel.app/'
